679590b0ef339b2c30b1758a06b0bbc0ad91864b
Malte Jürgens <maltejur@web.de>
2022-03-11 09:04:59 +0100

    add status

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,4 +1,4 @@
-import { Button, Paper, Typography } from "@material-ui/core";
+import { Button, MenuItem, Paper, Select, Typography } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 import React, { useState } from "react";
 import type { QueryExecResult } from "sql.js";
@@ -7,19 +7,37 @@ import SqlTable from "../components/SqlTable";
 import SqlTables from "../components/SqlTables";
 import useSql from "../hooks/sql";
 
+const databases = [
+  "/Chinook_Sqlite.sqlite",
+  "/Northwind_small.sqlite",
+  "/Northwind_large.sqlite",
+];
+
 export default function Editor() {
   const [query, setQuery] = useState("");
   const [status, setStatus] = useState<{
     sqlResult?: QueryExecResult[];
     error?: string;
     message?: string;
+    time?: number;
   }>({});
-  const { tables, exec } = useSql("/Chinook_Sqlite.sqlite");
+  const [database, setDatabase] = useState(databases[0]);
+  const { tables, exec } = useSql(database);
 
   return (
     <div>
       <Typography variant="h4" style={{ marginBottom: 20 }}>
-        Editor
+        Editor{" "}
+        <Select
+          value={database}
+          onChange={(event) => setDatabase(event.target.value as string)}
+        >
+          {databases.map((database) => (
+            <MenuItem key={database} value={database}>
+              {database}
+            </MenuItem>
+          ))}
+        </Select>
       </Typography>
       <Paper style={{ padding: 15, marginBottom: 30 }}>
         <SqlTables tables={tables} includeAll style={{ margin: 0 }} />
@@ -36,14 +54,16 @@ export default function Editor() {
           <Button
             onClick={() => {
               try {
+                let time = Date.now();
                 const sqlResult = exec([query]);
+                time = Date.now() - time;
                 downloadString(
                   JSON.stringify(sqlResult),
                   "application/json",
                   "expected.json"
                 );
-                setStatus({ message: "Download started", sqlResult });
-              } catch (error) {
+                setStatus({ message: "Download started", sqlResult, time });
+              } catch (error: any) {
                 setStatus({ error: error.message });
               }
             }}
@@ -55,9 +75,11 @@ export default function Editor() {
           <Button
             onClick={() => {
               try {
+                let time = Date.now();
                 const sqlResult = exec([query]);
-                setStatus({ sqlResult });
-              } catch (error) {
+                time = Date.now() - time;
+                setStatus({ sqlResult, time });
+              } catch (error: any) {
                 setStatus({ error: error.message });
               }
             }}
@@ -72,6 +94,13 @@ export default function Editor() {
       <div style={{ marginTop: 30 }}>
         {status.message && <Alert>{status.message}</Alert>}
         {status.error && <Alert severity="error">{status.error}</Alert>}
+        {!!status.sqlResult &&
+          status.sqlResult[0] &&
+          status.time !== undefined && (
+            <Alert severity="info">
+              {status.sqlResult[0].values.length} rows in {status.time / 1000}s
+            </Alert>
+          )}
         {status.sqlResult?.map((queryResult, index) => (
           <SqlTable key={index} table={queryResult} />
         ))}
